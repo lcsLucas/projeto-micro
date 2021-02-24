dcc28a9bac23a2ddc743484b740f185a1befb66e
N-ANGLO-5165\Lucas <lucas.tarta@hotmail.com>
2021-02-24 17:11:52 -0300

    adicionando metricas e atualizado o diagrama da estrutura do projeto

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -1,10 +1,34 @@
 package middlewares
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-kit/kit/metrics"
+	"github.com/go-kit/kit/metrics/prometheus"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+)
 
 func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		next(w, r)
+	}
+}
+
+func Metrics(next http.HandlerFunc) http.HandlerFunc {
+	var dur metrics.Histogram = prometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "myservice",
+		Subsystem: "api",
+		Name:      "request_duration_seconds",
+		Help:      "Total time spent serving requests.",
+	}, []string{})
+
+	defer func(begin time.Time) {
+		dur.Observe(time.Since(begin).Seconds())
+	}(time.Now())
+
+	return func(w http.ResponseWriter, r *http.Request) {
 		next(w, r)
 	}
 }
--- go.mod
+++ go.mod
@@ -8,10 +8,11 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
 	github.com/joho/godotenv v1.3.0
+	github.com/prometheus/client_golang v1.9.0
+	github.com/prometheus/procfs v0.6.0 // indirect
 	go.mongodb.org/mongo-driver v1.4.6
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
-	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 	golang.org/x/text v0.3.5 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/genproto v0.0.0-20210207032614-bba0dbe2a9ea // indirect
